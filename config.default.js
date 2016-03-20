7f4dc8c3b52e12e0bdb54364c532bb9de58790f6
Denis Rykovanov <drykovanov@wiley.com>
2016-03-20 17:18:49 +0300

    [+] report generation for week

--- config.default.js
+++ config.default.js
@@ -4,11 +4,12 @@ module.exports = {
         "host": "jira.wiley.ru",
         "apiVersion": "2",
         "projectKey": "as",
-        "maxIssueResults": 100,
+        "maxIssueResults": 10000,
         "verbose": true,
-        searchDateFormat: "YYYY/MM/DD HH:mm",
+        searchDateFormat: "YYYY/MM/DD HH:mm"
     },
     cmdArgDateFormat: "YYYY/MM/DD HH:mm",
-    reportDateFormat: "HH:mm DD/MM/YY",
+    reportDateFormat: "YY/MM/DD HH:mm",
+    repFileNameDate: "YY-MM-DD_HH-mm",
     startWorkingHour: 8
 };
--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 //thanks to https://github.com/B-Stefan/node-jira-worklog-export
 const moment = require('moment');
-const Promise = require('bluebird');
 const argvToRepDate = require('./lib/utils').argvToRepDate;
+const config = require('./lib/utils').config;
 const argv = require('optimist').argv;
-const _ = require('lodash');
 const generateReport = require('./lib/generate-report');
+const writeExcelReport = require('./lib/excel-report-writter').writeExcelReport;
+
 var startDate, endDate;
 
 function writeReport(dates) {
-    generateReport(dates).then( (result) => {
-        console.dir(result.summary);
-        var details = _.flatten(_.map(result.details, (items, author) => (
-                items.map( (item) => ({ author: author, comment: item.comment, issueKey: item.issue.key })) )));
-        console.dir(details);
+    generateReport(dates).then((result) => {
+        var filename = `time-${dates.startDate.format(config.repFileNameDate)}—to—${dates.endDate.format(config.repFileNameDate)}`;
+        writeExcelReport(filename, result)
     });
 }
 
@@ -20,11 +19,13 @@ if (argv.day) {
     startDate = argvToRepDate(argv.day);
     endDate = startDate.clone();
     endDate.add(1, 'd');
-    writeReport({startDate, startDate});
+    writeReport({startDate, endDate});
 } else if (argv.startDate && argv.endDate) {
     startDate = argvToRepDate(argv.startDate);
     endDate = argvToRepDate(argv.endDate);
     endDate.add(1, 'd');
     writeReport({startDate, endDate});
+} else {
+    console.log('no action specified')
 }
 
