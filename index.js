eaad0cd391a88f592eca53a079b3bd0d897ef2bb
Denis Rykovanov <drykovanov@wiley.com>
2016-03-21 10:18:20 +0300

    [+] report generation for yesterday, today, prev week

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 //thanks to https://github.com/B-Stefan/node-jira-worklog-export
 const moment = require('moment');
 const argvToRepDate = require('./lib/utils').argvToRepDate;
+const adjustWorkingTime = require('./lib/utils').adjustWorkingTime;
 const config = require('./lib/utils').config;
 const argv = require('optimist').argv;
 const generateReport = require('./lib/generate-report');
@@ -15,17 +16,66 @@ function writeReport(dates) {
     });
 }
 
-if (argv.day) {
-    startDate = argvToRepDate(argv.day);
-    endDate = startDate.clone();
+function reportForDay(startDate) {
+    var endDate = startDate.clone();
     endDate.add(1, 'd');
-    writeReport({startDate, endDate});
-} else if (argv.startDate && argv.endDate) {
-    startDate = argvToRepDate(argv.startDate);
-    endDate = argvToRepDate(argv.endDate);
-    endDate.add(1, 'd');
-    writeReport({startDate, endDate});
-} else {
-    console.log('no action specified')
+    writeReport({
+        startDate:  adjustWorkingTime(startDate),
+        endDate:    adjustWorkingTime(endDate)});
 }
 
+function reportForDuration(startDate, endDate) {
+    var shftEndDate = endDate.clone();
+    shftEndDate.add(1, 'd');
+    writeReport({
+        startDate:  adjustWorkingTime(startDate),
+        endDate:    adjustWorkingTime(shftEndDate)});
+}
+
+function reportToday() {
+    reportForDay(moment());
+}
+
+function reportYesterday() {
+    var yesterday = moment().subtract(1, 'd');
+    reportForDay(yesterday);
+}
+
+function reportWeek() {
+    reportForDuration(moment().startOf('isoWeek'), moment().endOf('isoWeek'));
+}
+
+function reportPrevWeek() {
+    reportForDuration(
+        moment().startOf('isoWeek').subtract(1, 'w'),
+        moment().endOf('isoWeek').subtract(1, 'w'));
+}
+
+switch (argv.cmd) {
+    case 'day':
+        startDate = argvToRepDate(argv.day);
+        reportForDay(startDates);
+        break;
+    case 'period':
+        startDate = argvToRepDate(argv.startDate);
+        endDate = argvToRepDate(argv.endDate);
+        reportForDuration(startDate, endDate);
+        break;
+    case 'today':
+        reportToday();
+        break;
+    case 'yesterday':
+        reportYesterday();
+        break;
+    case 'prevWeek':
+        reportPrevWeek();
+        break;
+    case 'week':
+        reportWeek();
+        break;
+    case undefined:
+        console.error(`cmd argument must be specified`);
+        break;
+    default:
+        throw new Error(`Unknown action ${argv.cmd}`)
+}
